80bbf2eabd533e699f1ef489793d1ba35a7e62f4
Fredy Izquierdo <fredyizquierdo.dev@gmail.com>
2026-07-20 21:27:18 +0200

    Primer proyecto practico con react

---
+++ 00-hola-mundo/src/App.jsx
@@ -0,0 +1,17 @@
+export default function App() {
+    return (
+        <article>
+            <header>
+                <img src="https://unavatar.io/github/kikobeats" alt="El avatar de kikobeats" />
+                <div>
+                    <strong>Kikobeats</strong>
+                    <span>@kikobeats</span>
+                </div>
+            </header>
+
+            <aside>
+                <button>Seguir</button>
+            </aside>
+        </article>
+    )
+}
