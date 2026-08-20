7dc755ea5ddc70f2f14e8eb785b52ad24e9e4f1d
Fredy Izquierdo <fredyizquierdo.dev@gmail.com>
2026-08-20 20:02:18 +0200

    Agregar constates de las APIs

--- react-prueba-tecnica/README.md
+++ react-prueba-tecnica/README.md
@@ -4,7 +4,9 @@ APIs:
 - Facts Random: https://catfact.ninja/fact
 - Imagen random: https://cataas.com/cat/says/hello
 
-Recupera un hecho aleatorio de gatos de la primera API y muestra una imagen de un gato con la primera palabra del hecho recuperado usando la segunda API.
+- Recupera un hecho aleatorio de gatos de la primera API
+- Recuperar la primera palabra del hecho 
+- Muestra una imagen de un gato con la primera palabra del hecho recuperado usando la segunda API.
 
 
 NOTAS:
@@ -16,4 +18,4 @@ NOTAS:
 
 Avance
 https://www.youtube.com/watch?v=XYpadB4VadY
-19:32
+42:32
--- react-prueba-tecnica/src/App.jsx
+++ react-prueba-tecnica/src/App.jsx
@@ -1,5 +1,27 @@
+import { useState, useEffect } from 'react'
+
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+//const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
+
 export function App () {
+    const [fact, setFact] = useState('lorem ipsum cat fact whatever')
+
+    useEffect(() => {
+        fetch(CAT_ENDPOINT_RANDOM_FACT)
+            .then(res => res.json())
+            .then(data => {
+                const { fact } = data
+                setFact(fact)
+
+                const firstWord = fact.split(' ')[0]
+            })
+    }, [])
+
     return (
-        <h1>App de gatitos</h1>
+        <main>
+            <h1>App de gatitos</h1>
+            {fact && <p>{fact}</p>}
+            
+        </main>
     )
 }
